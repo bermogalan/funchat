da175a43983af36a3318d206eba42416cdc70c74
Bermudes Galan <bgalan@besixgroup.com>
2021-08-04 13:27:45 +0800

    Additional unit test

--- UnitTests/Kritner.OrleansGettingStarted.Grains.Tests/ChatRoomTest.cs
+++ UnitTests/Kritner.OrleansGettingStarted.Grains.Tests/ChatRoomTest.cs
@@ -132,7 +132,31 @@ namespace Kritner.OrleansGettingStarted.Grains.Test
         }
 
         [Fact]
-        public async Task ChatRoomGetMessagesTest()
+        public async Task ChatRoomGetMessages50Test()
+        {
+            IChatRoom grain = await Silo.CreateGrainAsync<ChatRoom>("id");
+
+            // get a brand new grain to test
+            var userName = "test";
+            var chatRoomId = "id";
+            var chatRoomPassword = "pass";
+            var message = "hello!";
+
+            var roomCreated = await grain.CreateRoom(userName, chatRoomId, chatRoomPassword);
+            var enteredRoom = await grain.EnterRoom(userName, chatRoomId, chatRoomPassword);
+
+            for (int i = 0; i < 50; i++)
+            {
+                var sentSuccessfully1 = await grain.SendMessage(userName, chatRoomId, message);
+            }
+
+            var messages = await grain.GetMessages(50, chatRoomId);
+            
+            Assert.True(messages.Count == 50);
+        }
+
+        [Fact]
+        public async Task ChatRoomReadMessagesFromDateTest()
         {
             IChatRoom grain = await Silo.CreateGrainAsync<ChatRoom>("id");
 
@@ -150,8 +174,112 @@ namespace Kritner.OrleansGettingStarted.Grains.Test
             var sentSuccessfully3 = await grain.SendMessage(userName, chatRoomId, message);
 
             var messages = await grain.ReadMessagesFromDate(userName, chatRoomId, dateTimeStarted);
-            
+
             Assert.True(messages.Count == 4);
+        }
+
+        [Fact]
+        public async Task ChatRoomGetUsersTest()
+        {
+            IChatRoom grain = await Silo.CreateGrainAsync<ChatRoom>("id");
+
+            // get a brand new grain to test
+            var userName = "test";
+            var chatRoomId = "id";
+            var chatRoomPassword = "pass";
+            var dateTimeStarted = DateTime.Now;
+
+            var roomCreated = await grain.CreateRoom(userName, chatRoomId, chatRoomPassword);
+
+            for (int i = 0; i < 5; i++)
+            {
+                var enteredRoom = await grain.EnterRoom($"{userName}{i}", chatRoomId, chatRoomPassword);
+            }
+
+            var users = await grain.GetUsers(chatRoomId);
+
+            Assert.True(users.Count == 5);
+        }
+
+        [Fact]
+        public async Task ChatRoomGetChatRoomsTest()
+        {
+            IChatRoom grain = await Silo.CreateGrainAsync<ChatRoom>("id");
+
+            // get a brand new grain to test
+            var userName = "test";
+            var chatRoomId = "id";
+            var chatRoomPassword = "pass";
+
+            //Create room for user x2
+            for (int u = 0; u < 2; u++)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    var roomCreated = await grain.CreateRoom($"{userName}{u}", $"{chatRoomId}{u}{i}", chatRoomPassword);
+                }
+            }
+
+            var allChatRooms = await grain.GetChatRooms($"{userName}0", true);
+            var chatRooms = await grain.GetChatRooms($"{userName}0", false);
+
+            Assert.True(allChatRooms.Count == 10);
+            Assert.True(chatRooms.Count == 5);
+        }
+
+        [Fact]
+        public async Task ChatRoomGetChatRoomsIsAdminTest()
+        {
+            IChatRoom grain = await Silo.CreateGrainAsync<ChatRoom>("id");
+
+            // get a brand new grain to test
+            var userName = "test";
+            var chatRoomId = "id";
+            var chatRoomPassword = "pass";
+
+            //Create room for user x2
+            for (int u = 0; u < 2; u++)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    var roomCreated = await grain.CreateRoom($"{userName}{u}", $"{chatRoomId}{u}{i}", chatRoomPassword);
+                }
+            }
+
+            var allChatRooms = await grain.GetChatRooms(userName, true);
+
+            Assert.True(allChatRooms.Count == 10);
+        }
+
+        [Fact]
+        public async Task ChatRoomGetChatRoomsJoinedTest()
+        {
+            IChatRoom grain = await Silo.CreateGrainAsync<ChatRoom>("id");
+
+            // get a brand new grain to test
+            var userName = "test";
+            var chatRoomId = "id";
+            var userNameToJoin = "testJoiner";
+            var chatRoomPassword = "pass";
+
+            //Create room for user x2
+            for (int u = 0; u < 2; u++)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    var roomCreated = await grain.CreateRoom($"{userName}{u}", $"{chatRoomId}{u}{i}", chatRoomPassword);
+                    if (i == 3) 
+                    {
+                        var roomEntered = await grain.EnterRoom(userNameToJoin, $"{chatRoomId}{u}{i}", chatRoomPassword);
+                    }
+                }
+            }
+
+            var allChatRooms = await grain.GetChatRooms($"{userName}0", true);
+            var chatRoomsJoined = await grain.GetChatRoomsJoined(userNameToJoin, false);
+
+            Assert.True(allChatRooms.Count == 10);
+            Assert.True(chatRoomsJoined.Count == 2);
         }
     }
 }
